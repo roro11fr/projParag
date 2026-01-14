cf7a853f5b553d890ab1dc8a29bed52978d332ab
roro11fr <franciuc455@gmail.com>
2026-01-14 14:34:43 +0200

    add cors configuration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.v1.admin.user_router import router as user_router
 from app.api.v1.admin.company_router import router as company_router
 from app.api.v1.admin.client_router import router as client_router
@@ -8,6 +10,18 @@ from app.api.v1.plan_router import router as plan_router
 from app.api.v1.admin.plan_router import router as admin_plan_router
 
 app = FastAPI(title="proj_parag")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # Vite
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(company_router)
 app.include_router(client_router)
